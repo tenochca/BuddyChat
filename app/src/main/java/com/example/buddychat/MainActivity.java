88e887cc34ab914281c2a55ba8376214e78edab2
Diego Izaguirre <tenochca.di@outlook.com>
2025-07-10 21:15:56 -0500

    Set up MainActivity and MainApplication files

---
+++ app/src/main/java/com/example/buddychat/MainActivity.java
@@ -0,0 +1,7 @@
+package com.example.buddychat;
+import com.bfr.buddysdk.BuddyActivity;
+import com.bfr.buddysdk.BuddySDK;
+
+public class MainActivity extends BuddyActivity {
+
+}
---
+++ app/src/main/java/com/example/buddychat/MainApplication.java
@@ -0,0 +1,9 @@
+package com.example.buddychat;
+import com.bfr.buddysdk.BuddyApplication;
+
+public class MainApplication extends BuddyApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
