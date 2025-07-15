7bfe1f0796b8e7c930373e568c8e19d0a827b7d1
Diego Izaguirre <tenochca.di@outlook.com>
2025-07-14 23:03:24 -0500

    edited SttListener interface to include more methods

--- app/src/main/java/com/example/buddychat/SttListener.java
+++ app/src/main/java/com/example/buddychat/SttListener.java
@@ -1,5 +1,15 @@
 package com.example.buddychat;
 
 public interface SttListener {
-    void onSpeechResult(String text);
+    //called when speech is successfully recognized
+    void onSpeechResult(String utterance, float confidence);
+
+    void onError(String errorMessage);
+
+    //called when the STT engine is ready to listen
+    void onSttReady();
+
+    void onSttPaused();
+
+    void onSttStopped();
 }
