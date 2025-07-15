f6566f2b66bd34684fa5336ac92a84371950f330
Diego Izaguirre <tenochca.di@outlook.com>
2025-07-14 23:30:02 -0500

    edited SpeechToTextService interface to include more methods

--- app/src/main/java/com/example/buddychat/SpeechToTextService.java
+++ app/src/main/java/com/example/buddychat/SpeechToTextService.java
@@ -1,6 +1,27 @@
 package com.example.buddychat;
+import java.util.Locale;
 
 public interface SpeechToTextService {
-    void startListening(SttListener listener);
+
+    //Enum for STT Engine selection
+    enum SttEngineType {
+        GOOGLE,
+        CERENCE_FREE_SPEECH,
+        CERENCE_LOCAL_FCF
+    }
+
+    //Creates and configures STT task for a specific engine and locale
+    void prepareSTTEngine(SttEngineType engineType, Locale locale);
+
+    //initializes the created STT task
+    void initializeListening(SttListener listener);
+
+    //starts the listening process, this assumes initializeListening has been called
+    void startListening(boolean listenContinuously);
+
+    void pauseListening();
+
     void stopListening();
+
+    void releaseService();
 }
