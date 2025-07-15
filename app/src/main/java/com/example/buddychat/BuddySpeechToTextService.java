fa613c2979a8b2e8804b761725ae71fe13eecd0e
Diego Izaguirre <tenochca.di@outlook.com>
2025-07-15 00:14:20 -0500

    edited BuddySpeechToTextService.java to implement new methods

--- app/src/main/java/com/example/buddychat/BuddySpeechToTextService.java
+++ app/src/main/java/com/example/buddychat/BuddySpeechToTextService.java
@@ -7,16 +7,42 @@ import com.bfr.buddysdk.BuddySDK;
 import com.bfr.buddysdk.services.speech.STTTask;
 import com.example.buddychat.SpeechToTextService;
 
+import android.content.res.AssetManager; // Required for Cerence local FCF
+import android.os.RemoteException;
+import android.util.Log;
+
+import java.util.Locale;
+
 public class BuddySpeechToTextService implements SpeechToTextService {
 
 
     @Override
-    public void startListening(SttListener listener) {
+    public void prepareSTTEngine(SttEngineType engineType, Locale locale) {
+
+    }
+
+    @Override
+    public void initializeListening(SttListener listener) {
+
+    }
+
+    @Override
+    public void startListening(boolean listenContinuously) {
+
+    }
+
+    @Override
+    public void pauseListening() {
 
     }
 
     @Override
     public void stopListening() {
+
+    }
+
+    @Override
+    public void releaseService() {
 
     }
 }
