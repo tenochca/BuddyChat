e7077f7f6fd870b4018541b772451e3f63bca66c
Diego Izaguirre <tenochca.di@outlook.com>
2025-07-21 23:45:06 -0500

    Implement `SttListener` interface in `MainActivity`

--- app/src/main/java/com/example/buddychat/MainActivity.java
+++ app/src/main/java/com/example/buddychat/MainActivity.java
@@ -2,6 +2,36 @@ package com.example.buddychat;
 import com.bfr.buddysdk.BuddyActivity;
 import com.bfr.buddysdk.BuddySDK;
 
-public class MainActivity extends BuddyActivity {
+import com.example.buddychat.BuddySpeechToTextService;
+import com.example.buddychat.SpeechToTextService;
+import com.example.buddychat.SttListener;
 
+import java.util.Locale;
+
+public class MainActivity extends BuddyActivity implements SttListener {
+
+    @Override
+    public void onSpeechResult(String utterance, float confidence) {
+
+    }
+
+    @Override
+    public void onError(String errorMessage) {
+
+    }
+
+    @Override
+    public void onSttReady() {
+
+    }
+
+    @Override
+    public void onSttPaused() {
+
+    }
+
+    @Override
+    public void onSttStopped() {
+
+    }
 }
