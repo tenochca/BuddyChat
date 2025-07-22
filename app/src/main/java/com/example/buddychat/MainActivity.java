a542af05ab0a152fd9fe41c89ca2803147942181
Diego Izaguirre <tenochca.di@outlook.com>
2025-07-21 23:57:09 -0500

    Updated imports and added instance variables to MainActivity
    The MainActivity.java file now imports Button, CheckBox, Spinner, and TextView from the Android widget library.
    
    multiple instance variables have been added to the MainActivity class:

--- app/src/main/java/com/example/buddychat/MainActivity.java
+++ app/src/main/java/com/example/buddychat/MainActivity.java
@@ -1,4 +1,9 @@
 package com.example.buddychat;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 import com.bfr.buddysdk.BuddyActivity;
 import com.bfr.buddysdk.BuddySDK;
 
@@ -9,6 +14,34 @@ import com.example.buddychat.SttListener;
 import java.util.Locale;
 
 public class MainActivity extends BuddyActivity implements SttListener {
+
+    private static final String TAG = "MainActivity";
+
+    private SpeechToTextService sttService;
+    private TextView textViewRecognizedText;
+    private TextView textViewStatus;
+    private Button buttonToggleListen;
+    private Button buttonPrepareEngine;
+    private Spinner spinnerSttEngine;
+    private Spinner spinnerLanguage;
+    private CheckBox checkboxContinuousListen;
+
+
+    private boolean isListening = false;
+    private boolean isEnginePrepared = false;
+    private boolean isEngineInitialized = false;
+
+    private SpeechToTextService.SttEngineType selectedEngineType = SpeechToTextService.SttEngineType.GOOGLE;
+    private Locale selectedLocale = Locale.ENGLISH; // Default
+
+    private final String[] sttEngineDisplayItems = {"Google", "Cerence Free Speech", "Cerence Local .fcf"};
+    private final SpeechToTextService.SttEngineType[] sttEngineEnumItems = {
+            SpeechToTextService.SttEngineType.GOOGLE,
+            SpeechToTextService.SttEngineType.CERENCE_FREE_SPEECH,
+            SpeechToTextService.SttEngineType.CERENCE_LOCAL_FCF
+    };
+
+    private final Locale[] languageItems = {Locale.ENGLISH, Locale.FRENCH};
 
     @Override
     public void onSpeechResult(String utterance, float confidence) {
