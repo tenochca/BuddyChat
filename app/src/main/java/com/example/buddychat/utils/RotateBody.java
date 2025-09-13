ec78155732945fae4e8f33eca7ae2280eae03e8b
amurphy99 <amurphy62299@gmail.com>
2025-09-13 10:49:57 -0500

    Actual movement disabled
    While testing, disabling the codes ability to actually move Buddy. Also set it up to change buddy's emotions instead of move it.

--- app/src/main/java/com/example/buddychat/utils/RotateBody.java
+++ app/src/main/java/com/example/buddychat/utils/RotateBody.java
@@ -17,11 +17,11 @@ public class RotateBody {
     // --------------------------------------------------------------------
     // Speed  : in deg/s (>0 to go forward, <0 to go backward)
     // Degree : degree of rotation
-    // ToDO: Specifics here probably need to be adjusted... like callbacks should be given etc.
-    /** Rotate Buddy a set number of degrees at a set speed */
+    // ToDo: Specifics here probably need to be adjusted... like callbacks should be given etc.
     public static void Rotate(float speed, float degree) {
         Log.d(TAG, String.format("Attempting to rotate Buddy %.3f degrees at %.3f speed", degree, speed));
 
+        /* ToDo: Commenting this out for now so we don't actually move. Want to make sure other stuff works first.
         // Check if wheels are enabled
         if (!wheelsEnabled) { EnableWheels(1, 1); }
 
@@ -32,6 +32,10 @@ public class RotateBody {
 
         // Disable wheels when done
         EnableWheels(0, 0);
+
+        */
+
+        Log.d(TAG, "--- Actual movement temporarily disabled... ---");
     }
 
     // --------------------------------------------------------------------
@@ -53,11 +57,9 @@ public class RotateBody {
     // --------------------------------------------------------------------
     // turnOnLeftWheel  : setting the Left  wheel motor on On of "int" type (On=1) (Off=0)
     // turnOnRightWheel : setting the right wheel motor on On of "int" type (On=1) (Off=0)
-    /** Toggle wheel motors of the robot */
     public static void EnableWheels(int turnOnLeftWheel, int turnOnRightWheel) {
-        Log.d(TAG, String.format("Toggling wheels -- left: %d, right: %d", turnOnLeftWheel, turnOnRightWheel));
-
         boolean enabling = (turnOnLeftWheel == 1) & (turnOnRightWheel == 1);
+        Log.d(TAG, String.format("Toggling wheels -- left: %d, right: %d", turnOnLeftWheel, turnOnRightWheel));
 
         // ToDo: Not sure why it says the method is deprecated... SDK documentation shows it...
         BuddySDK.USB.enableWheels(turnOnLeftWheel, turnOnRightWheel, new IUsbCommadRsp.Stub() {
@@ -65,7 +67,6 @@ public class RotateBody {
             @Override public void onFailed (String s) { Log.d(TAG, "EnableWheels failed: "  + s); }
         });
     }
-
 
     // No instances
     private RotateBody() {}
