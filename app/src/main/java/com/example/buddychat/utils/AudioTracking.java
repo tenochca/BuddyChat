a51331dc3071eb5ef8eb4aa032a98d8e41199737
amurphy99 <amurphy62299@gmail.com>
2025-09-13 10:51:02 -0500

    Changed the way it was checking the sensors
    Before it used a thread with an infinite loop. This still probably isn't fully correct though, need to try subscribing to the sensors.

--- app/src/main/java/com/example/buddychat/utils/AudioTracking.java
+++ app/src/main/java/com/example/buddychat/utils/AudioTracking.java
@@ -1,28 +1,29 @@
 package com.example.buddychat.utils;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 import com.bfr.buddy.usb.shared.IUsbCommadRsp;
 import com.bfr.buddysdk.BuddySDK;
 
-import com.example.buddychat.utils.RotateBody;
-
 // ====================================================================
 // AudioTracking
 // ====================================================================
-// TODO: I feel like the RemoteException stuff is to make the while loop not crash the app? not sure...
+// ToDo: This implementation is based on the sample projects they have on GitHub. But the SDK shows a way to actually subscribe to the sensors...
 public class AudioTracking {
-    private static final String TAG          = "DPU_AudioTracking";
-    private static final int    MIN_DECIBELS = 40;
-    private static final int    MIN_ANGLE    = 40;
+    private static final String TAG = "DPU_AudioTracking";
+
+    private static final int MIN_DECIBELS       = 40;
+    private static final int MIN_ANGLE          = 150;
+    private static final int UPDATE_INTERVAL_MS = 3_000;
+
+    private static          Handler handler;
+    private static volatile boolean enabled = false;
 
     // Microphone utility
     static float AmbientNoise  = 0; // Sound volume in Decibels
     static float LocationAngle = 0; // Degree location of the sound
-
-    // Run the tracking process in a thread
-    static boolean enabled  = true;
-    static Thread SensorsTh = new Thread(AudioTracking::trackContinuously);
 
     // --------------------------------------------------------------------
     // Setup Continuous Tracking
@@ -33,7 +34,7 @@ public class AudioTracking {
 
         // After SDK launches we can setup the sensors
         BuddySDK.USB.enableSensorModule(true, new IUsbCommadRsp.Stub() {
-            @Override public void onSuccess(String s) { Log.i(TAG, "Enabled Sensors"            ); toggleTracking(false); }
+            @Override public void onSuccess(String s) { Log.i(TAG, "-------------- Enabled Sensors --------------"); toggleTracking(true); }
             @Override public void onFailed (String s) { Log.i(TAG, "Fail to Enable sensors:" + s); }
         });
     }
@@ -41,21 +42,47 @@ public class AudioTracking {
     // --------------------------------------------------------------------
     // Thread Control
     // --------------------------------------------------------------------
-    public static void trackContinuously() { while (enabled) updateSensors(); }
-    // TODO: better way to do toggling here, I don't want to double start the thread
-    public static void toggleTracking(boolean toggleOff) {
-        if (toggleOff) { enabled = false;                   }
-        else           { enabled = true; SensorsTh.start(); }
+    /** Runs the continuous checks for the sensors. */
+    private static final Runnable trackingRunnable = new Runnable() {
+        @Override public void run() { if (enabled) { updateSensors(); handler.postDelayed(this, UPDATE_INTERVAL_MS); }}
+    };
+
+    /** Turn the audio tracking on or off */
+    public static void toggleTracking(boolean enable) {
+        // Enable everything
+        if (enable) {
+            if (!enabled) { enabled = true;
+                if (handler == null) { handler = new Handler(Looper.getMainLooper()); }
+                handler.post(trackingRunnable);
+            }
+        }
+        // Disable tracking
+        else { if (enabled) { enabled = false; handler.removeCallbacks(trackingRunnable); } }
     }
+
+
+    // --------------------------------------------------------------------
+    // Check the sensors; act accordingly
+    // --------------------------------------------------------------------
     // ToDo: Should use callbacks that disable the audio tracking while it is moving and re-enables it once the Rotate call is completed.
     public static void updateSensors() {
-        AmbientNoise = BuddySDK.Sensors.Microphone().getAmbiantSound();
-        if (AmbientNoise > MIN_DECIBELS) {
-            LocationAngle = BuddySDK.Sensors.Microphone().getSoundLocalisation();
-            Log.d(TAG, String.format("Ambient dB: %s, Angle: %s", AmbientNoise, LocationAngle));
+        AmbientNoise  = BuddySDK.Sensors.Microphone().getAmbiantSound();
+        LocationAngle = BuddySDK.Sensors.Microphone().getSoundLocalisation();
 
+        if ((AmbientNoise > MIN_DECIBELS) & (LocationAngle > 0)) {
+            Log.i(TAG, String.format("Ambient dB: %s, Angle: %s", AmbientNoise, LocationAngle));
+
+            // ToDo: Temporarily calling the Emotions code here to test if it works
             // If noise is loud enough and angle is different enough, rotate buddy to face the source
-            if (LocationAngle > MIN_ANGLE) { RotateBody.Rotate(30, LocationAngle); }
+            if (LocationAngle > MIN_ANGLE) {
+                RotateBody.Rotate(10, LocationAngle);
+                //Emotions.setPositivityEnergy((float) 0.1, (float) 0.1);
+                Emotions.setMood(true);
+            }
+            else {
+                //Emotions.setPositivityEnergy((float) 0.9, (float) 0.9);
+                Emotions.setMood(false);
+            }
         }
     }
 
