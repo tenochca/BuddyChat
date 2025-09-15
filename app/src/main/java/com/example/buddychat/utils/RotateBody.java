884f4d6631ad9678376c67e616bf6293265987a7
amurphy99 <amurphy62299@gmail.com>
2025-09-14 20:18:35 -0500

    updated method & cleaned up code

--- app/src/main/java/com/example/buddychat/utils/RotateBody.java
+++ app/src/main/java/com/example/buddychat/utils/RotateBody.java
@@ -9,62 +9,51 @@ import com.bfr.buddysdk.BuddySDK;
 // Rotate the body of the robot
 // ====================================================================
 public class RotateBody {
-    private static final String  TAG           = "DPU_RotateBody";
-    public  static       boolean wheelsEnabled = false;
+    private static final String TAG = "[DPU_RotateBody]";
+    public  static boolean wheelsEnabled = false;
 
     // --------------------------------------------------------------------
     // Rotate Buddy a set number of degrees at a set speed
     // --------------------------------------------------------------------
     // Speed  : in deg/s (>0 to go forward, <0 to go backward)
     // Degree : degree of rotation
-    // ToDo: Specifics here probably need to be adjusted... like callbacks should be given etc.
+    // ToDo: Specifics here probably need to be adjusted... like callbacks should be given for onCompletion, etc.
     public static void Rotate(float speed, float degree) {
-        Log.d(TAG, String.format("Attempting to rotate Buddy %.3f degrees at %.3f speed", degree, speed));
+        Log.d(TAG, String.format("%s Attempting to rotate Buddy %.3f degrees at %.3f speed", TAG, degree, speed));
 
         /* ToDo: Commenting this out for now so we don't actually move. Want to make sure other stuff works first.
-        // Check if wheels are enabled
-        if (!wheelsEnabled) { EnableWheels(1, 1); }
-
+        // Check if wheels are enabled first
+        if (!wheelsEnabled) { EnableWheels(true); }
         BuddySDK.USB.rotateBuddy(speed, degree, new IUsbCommadRsp.Stub() {
             @Override public void onSuccess(String s) { Log.d(TAG, "Rotate success: " + s); }
             @Override public void onFailed (String s) { Log.d(TAG, "Rotate failed:  " + s); }
         });
-
-        // Disable wheels when done
-        EnableWheels(0, 0);
-
+        EnableWheels(false); // Disable wheels when done
         */
 
-        Log.d(TAG, "--- Actual movement temporarily disabled... ---");
+        Log.d(TAG, String.format("%s Actual movement temporarily disabled...", TAG));
     }
 
     // --------------------------------------------------------------------
     // Emergency stop for the motors
     // --------------------------------------------------------------------
-    /** Emergency stop for the motors */
+    /** Emergency stop for the motors (Stop motors AND disable wheels..?). */
     public static void StopMotors() {
         BuddySDK.USB.emergencyStopMotors(new IUsbCommadRsp.Stub() {
-            @Override public void onSuccess(String s) { Log.d(TAG, "StopMotors success: " + s); }
-            @Override public void onFailed (String s) { Log.d(TAG, "StopMotors failed: "  + s); }
+            @Override public void onSuccess(String s) { Log.i(TAG, String.format("%s StopMotors success: %s", TAG, s)); }
+            @Override public void onFailed (String s) { Log.w(TAG, String.format("%s StopMotors failed: %s",  TAG, s)); }
         });
-
-        // Stop motors AND disable wheels..?
-        EnableWheels(0, 0);
+        EnableWheels(false);
     }
 
     // --------------------------------------------------------------------
     // Toggle the wheel motors (enable or disable both happen here)
     // --------------------------------------------------------------------
-    // turnOnLeftWheel  : setting the Left  wheel motor on On of "int" type (On=1) (Off=0)
-    // turnOnRightWheel : setting the right wheel motor on On of "int" type (On=1) (Off=0)
-    public static void EnableWheels(int turnOnLeftWheel, int turnOnRightWheel) {
-        boolean enabling = (turnOnLeftWheel == 1) & (turnOnRightWheel == 1);
-        Log.d(TAG, String.format("Toggling wheels -- left: %d, right: %d", turnOnLeftWheel, turnOnRightWheel));
-
-        // ToDo: Not sure why it says the method is deprecated... SDK documentation shows it...
-        BuddySDK.USB.enableWheels(turnOnLeftWheel, turnOnRightWheel, new IUsbCommadRsp.Stub() {
-            @Override public void onSuccess(String s) { Log.d(TAG, "EnableWheels success: " + s); wheelsEnabled = enabling; }
-            @Override public void onFailed (String s) { Log.d(TAG, "EnableWheels failed: "  + s); }
+    public static void EnableWheels(boolean enable) {
+        Log.d(TAG, String.format("%s Toggling wheels %s", TAG, (enable ? "on" : "off")));
+        BuddySDK.USB.enableWheels(enable, new IUsbCommadRsp.Stub() {
+            @Override public void onSuccess(String s) { Log.d(TAG, String.format("%s EnableWheels success: %s", TAG, s)); wheelsEnabled = enable; }
+            @Override public void onFailed (String s) { Log.w(TAG, String.format("%s EnableWheels failed: %s",  TAG, s)); }
         });
     }
 
